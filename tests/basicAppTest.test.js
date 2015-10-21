d024038d6288b14c659c815acfe9f4bdce15fa49
Paul Allen <ptallen63@gmail.com>
2015-10-21 17:37:33 -0400

    setting up unit tests
    getting things set up for testing with karma and travis

---
+++ tests/basicAppTest.test.js
@@ -0,0 +1,9 @@
+describe('app', function() {
+  // Load the module with MainController
+  beforeEach(module('ngOutpost'));
+
+
+  it('Shouel be true', function() {
+  	expect(true).toBe(true);
+  })
+})
