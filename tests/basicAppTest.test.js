07d71cd19153f4c3dca1010e28d1fcda1bfb4c5a
Paul Allen <ptallen63@gmail.com>
2015-10-21 22:06:19 -0400

    fixesing some crazy rabit hold mistakes

--- tests/basicAppTest.test.js
+++
@@ -1,9 +0,0 @@
-describe('app', function() {
-  // Load the module with MainController
-  //beforeEach(module('ngOutpost'));
-
-
-  it('Shouel be true', function() {
-  	expect(true).toBe(true);
-  })
-})
